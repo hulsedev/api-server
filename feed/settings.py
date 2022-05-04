1b797847ab657d7ac75c081e45cbf57292323dd1
sachaStewart <sachalevy26@gmail.com>
2022-05-04 13:51:51 -0400

    add dependency for django-on-heroku

--- feed/settings.py
+++ feed/settings.py
@@ -186,3 +186,8 @@ EVENTSTREAM_CHANNELMANAGER_CLASS = "feedapp.channelmanager.ChannelAuthManager"
 
 # whitenoise related stuff
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# handle settings for django
+import django_on_heroku
+
+django_on_heroku.settings(locals())
