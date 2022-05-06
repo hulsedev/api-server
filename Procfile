6f45d44f314fc8082ed66a14a3bf93f4eff947ed
sachaStewart <sachalevy26@gmail.com>
2022-05-05 20:09:04 -0400

    edit procfile to remove wsgi

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn feed.wsgi
+web2: daphne feed.agsi:application --port $PORT --bind 0.0.0.0 -v2
 release: python manage.py makemigrations && python manage.py migrate
--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,6 +1,6 @@
-export $(cat .dev.env | xargs)
+export $(cat .env | xargs)
 
 python manage.py collectstatic --clear --noinput && \
     python manage.py makemigrations && \
     python manage.py migrate && \
-    python manage.py runserver
+    python manage.py runserver --verbosity=3
